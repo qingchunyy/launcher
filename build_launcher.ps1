e6bfd86d02edf7997677cc3ca599a497bc081868
qingchunyy <2656469055@qq.com>
2026-01-05 06:36:00 +0800

    Update build command to build without project name

--- build_launcher.ps1
+++ build_launcher.ps1
@@ -56,7 +56,7 @@ Write-Output "Current HEAD is at $Head in $Branch committed at $CTimeS"
 Write-Output "build"
 Set-Location ./project
 dotnet restore
-dotnet build SPT.Build
+dotnet build
 
 if ($LASTEXITCODE -ne 0) {
     throw ("dotnet build failed, exit code $LASTEXITCODE")
