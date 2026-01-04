7c547854e436deac2ef09e7668c96b0afc715ba2
qingchunyy <2656469055@qq.com>
2026-01-05 04:41:12 +0800

    Create build_launcher.ps1

---
+++ build_launcher.ps1
@@ -0,0 +1,65 @@
+Param(
+    [Parameter(Mandatory=$false)]
+    [Switch] $Overwrite,
+
+    [Parameter(Mandatory=$false)]
+    [string] $Branch
+)
+
+$ErrorActionPreference = "Stop"
+$SOURCE_DIR = "./Launcher"
+$SOURCE_REPO = "https://github.com/qingchunyy/launcher.git"
+
+$BuildOnCommit = $Commit.Length -gt 0
+
+if (Test-Path -Path $SOURCE_DIR) {
+    if ($Overwrite -or (Read-Host "$SOURCE_DIR exists, delete? [y/n]") -eq 'y') {
+        Write-Output "$SOURCE_DIR exists, removing"
+        Remove-Item -Recurse -Force $SOURCE_DIR
+    }
+    else
+    {
+        Exit 1
+    }
+}
+
+Write-Output "clone repo"
+if ( $Branch.Length -gt 0 )
+{
+    Write-Output "Cloning branch $Branch"
+    git clone --depth 1 -b $Branch $SOURCE_REPO $SOURCE_DIR
+} 
+else
+{
+    Write-Output "Branch not given, using default branch"
+    git clone --depth 1 $SOURCE_REPO $SOURCE_DIR
+}
+
+Set-Location $SOURCE_DIR
+
+if ($BuildOnCommit) {
+    Write-Output "Checking out the commit $Commit"
+    git fetch --depth=1 $SOURCE_REPO $Commit
+    git checkout $Commit
+
+    if ($LASTEXITCODE -ne 0) {
+        throw ("Commit $Commit checkout failed. It doesn't exist? git exit code $LASTEXITCODE")
+    }
+}
+
+$Head = git rev-parse --short HEAD
+$CTime = git log -1 --format="%at"
+$CTimeS = (([System.DateTimeOffset]::FromUnixTimeSeconds($CTime)).DateTime).ToString("yyyyMMddHHmmss")
+
+Write-Output "Current HEAD is at $Head in $Branch committed at $CTimeS"
+
+Write-Output "build"
+Set-Location ./project
+dotnet restore
+dotnet build SPT.Build
+
+if ($LASTEXITCODE -ne 0) {
+    throw ("dotnet build failed, exit code $LASTEXITCODE")
+}
+
+Get-ChildItem ./Build
